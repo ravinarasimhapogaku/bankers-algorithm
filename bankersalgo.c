74a1507395fcd1522391e085d6a2ff57c0708ef2
Ravi Narasimha <92355929+ravinarasimhapogaku@users.noreply.github.com>
2023-04-20 09:58:26 +0530

    Update bankersalgo.c

--- bankersalgo.c
+++ bankersalgo.c
@@ -19,9 +19,8 @@ int nProcessRan = 0;
 pthread_mutex_t lockResources;
 pthread_cond_t condition;
 
-// get safe sequence is there is one else return false
 bool getSafeSeq();
-// process function
+
 void* processCode(void* );
 
 int main(int argc, char** argv) {
@@ -55,15 +54,15 @@ int main(int argc, char** argv) {
         }
         printf("\n");
 
-	// maximum required resources
+	
         for(int i=0; i<nProcesses; i++) {
-                printf("\nMaximum resource required by process %d (R1 R2 ...)? ", i+1);
+                printf("\nMax resource required by process %d (R1 R2 ...)? ", i+1);
                 for(int j=0; j<nResources; j++)
                         scanf("%d", &maxRequired[i][j]);
         }
         printf("\n");
 
-	// calculate need matrix
+	
         need = (int **)malloc(nProcesses * sizeof(*need));
         for(int i=0; i<nProcesses; i++)
                 need[i] = (int *)malloc(nResources * sizeof(**need));
